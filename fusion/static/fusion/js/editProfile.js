a3cf24660d45a3d93285e9bfe1912a9dde493ff4
kmunshi97 <kmunshi.97@gmail.com>
2017-10-02 15:16:15 +0530

    Added MediaQuery and other minor changes in placement module!

--- fusion/static/fusion/js/editProfile.js
+++ fusion/static/fusion/js/editProfile.js
@@ -5,10 +5,10 @@ function editFirst(){
     var interestSpan = $("#interestSpan").text().trim();
     var contactSpan = $("#contactSpan").text().trim();
 
-    
+
     var buttonValue = $("#editButton").val()
-        
-    if(buttonValue == "Edit"){   
+
+    if(buttonValue == "Edit"){
         $("#editButton").val("Save");
 
         $("#contactInput").val(contactSpan);
@@ -55,4 +55,51 @@ function editFirst(){
 
     }
 
+}
+
+function editStudent() {
+    var aboutSpan = $("#aboutSpan").text().trim();
+    var contactSpan = $("#contactSpan").text().trim();
+    var interestSpan = $("#interestSpan").text().trim();
+
+    var buttonValue = $("#editButton").val()
+
+    if(buttonValue == "Edit") {
+        $("#editButton").val("Save");
+
+        $("#contactInput").val(contactSpan);
+        $("#contactInput").show();
+        $("#contactSpan").hide();
+        $("#contactIcon").hide();
+
+        $("#aboutTextarea").val(aboutSpan);
+        $("#aboutTextarea").show();
+        $("#aboutSpan").hide();
+
+        $("#interestTextarea").val(interestSpan);
+        $("#interestTextarea").show();
+        $("#interestSpan").hide();
+    }
+
+    else if($("#editButton").val("Save")) {
+        $("#editButton").val("Edit");
+
+        var contactValue = $("#contactInput").val().trim();
+        $("#contactSpan").text(contactValue);
+        $("#contactInput").hide();
+        $("#contactSpan").show();
+        $("#contactIcon").show();
+
+        var aboutSpan = $("#aboutTextarea").val().trim();
+        $("#aboutSpan").text(aboutSpan);
+        $("#aboutTextarea").hide();
+        $("#aboutSpan").show();
+
+        var interestSpan = $("#interestTextarea").val().trim();
+        $("#interestSpan").text(interestSpan);
+        $("#interestTextarea").hide();
+        $("#interestSpan").show();
+
+
+    }
 }
