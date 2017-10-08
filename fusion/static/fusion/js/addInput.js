e431e379acde3cf1279dbd35324d9358d80e46cf
ravuri0898 <ravuriabhignya@gmail.com>
2017-10-08 23:22:30 +0530

    finished developing frms

---
+++ fusion/static/fusion/js/addInput.js
@@ -0,0 +1,10 @@
+
+function addInput(){    
+    var newInput = document.createElement("input");
+        newInput.setAttribute("type", "text");
+        newInput.setAttribute("placeholder","Enter here!");
+
+    var parent = document.getElementById("addInputDiv");
+        parent.className = "ui input";
+        parent.appendChild(newInput);
+}
